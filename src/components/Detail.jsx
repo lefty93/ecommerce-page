191da46caa32b0a504c6aefc2b1565f43511e5e6
Lefty93 <yongchuseng@gmail.com>
2023-06-21 22:32:54 +0800

    .

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Detail = () => {
   return (
-    <div>Detail</div>
+    <div>
+      <div>
+        <h3>SNEAKER COMPANY</h3>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+      </div>
+      <div>
+        <h2>$125.00</h2>
+        <p>50%</p>
+        <div>
+          $250.00
+        </div>
+      </div>
+      <div>
+        <div>
+          <button>-</button>
+          <p>0</p>
+          <button>+</button>
+        </div>
+        <div>
+          Add to Cart
+        </div>
+      </div>
+    </div>
   )
 }
 
